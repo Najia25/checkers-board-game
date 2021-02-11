75f4ebfe9ab965042d4d58d8d8a850270050d991
Najia25 <najiaafrin2@gmail.com>
2021-02-11 19:19:37 +0600

    initial board-layout done

---
+++ src/components/Board.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="grid">
+    <div
+      class="row d-flex flex-nowrap"
+      v-for="x in 8"
+      :key="x"
+    >
+      <div
+        class="block"
+        v-for="y in 8"
+        :key="y"
+        :style="[(!(x % 2) && y % 2) || ((x % 2) && !(y % 2)) ? { background: 'red'} : null]"
+      >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Board',
+}
+</script>
+
+<style lang="scss" scoped>
+.block {
+  height: 55px;
+  width: 55px;
+  background: #ECF0F1;
+}
+.dark-background {
+  background: blue;
+}
+</style>
--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,12 +1,16 @@
 <template>
   <v-container>
+    <Board />
   </v-container>
 </template>
 
 <script>
+  import Board from './Board'
   export default {
     name: 'Game',
-
+    components: {
+      Board
+    },
     data: () => ({
     })
   }
